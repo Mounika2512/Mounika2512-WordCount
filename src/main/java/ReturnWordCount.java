25b2f64d09647381794b2de79b4b6a3871188d94
Mounika2512 <mounikasaviti25@gmail.com>
2024-04-05 19:40:04 +0530

    wordcount

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -1,10 +1,19 @@
-
 public class ReturnWordCount {
     /**
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
-    public int count(String in){
-        return 0;
+    public int count(String n){
+        if (n == null || n.isEmpty()) {
+                return 0;
+            }
+            String[] words = n.split("\\s+");
+            return words.length;
+        }
+    
+        public static void main(String[] args) {
+            ReturnWordCount r= new ReturnWordCount();
+            String sentence = "Hello Every one!";
+            System.out.println("Word count: " + r.count(sentence));
+        }
     }
-}
